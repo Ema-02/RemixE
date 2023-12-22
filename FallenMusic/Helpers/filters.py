f566733f6fe7c6e822db2d3198243e9dc068390e
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-22 22:57:44 +0300

    Update filters.py

--- FallenMusic/Helpers/filters.py
+++ FallenMusic/Helpers/filters.py
@@ -1,8 +1,6 @@
 from typing import List, Union
-from pyrogram import filters,Client
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardMarkup as Markup, InlineKeyboardButton as Button
-from pyrogram.enums import ChatType
 from pyrogram.errors import UserNotParticipant
 from FallenMusic import app
 
@@ -12,7 +10,7 @@ def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
 
 
-channel = "cczza"
+channel = "BENfiles" # Your Channel
 async def subscription(_, __: Client, message: Message):
     user_id = message.from_user.id
     try: await app.get_chat_member(channel, user_id)
@@ -20,16 +18,3 @@ async def subscription(_, __: Client, message: Message):
     return True
     
 subscribed = filters.create(subscription)
-
-@app.on_message(~subscribed)
-async def checker(_: Client, message: Message):
-    if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]: await message.delete()
-    user_id = message.from_user.id
-    user = message.from_user.first_name
-    markup = Markup([
-        [Button("- اشتراك -", url=f"https://t.me/{channel}")]
-    ])
-    await message.reply(
-        f"عذرًا عزيزي [{user}](tg://openmessage?user_id={user_id}) عليك الإشتراك بقناة البوت أولا.",
-        reply_markup = markup
-    )
