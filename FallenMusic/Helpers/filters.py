96b2f09d96060fafce16be69bdbafe00038b2426
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-22 23:45:15 +0300

    Update filters.py

--- FallenMusic/Helpers/filters.py
+++ FallenMusic/Helpers/filters.py
@@ -1,20 +1,8 @@
 from typing import List, Union
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.errors import UserNotParticipant
-from FallenMusic import app
+from pyrogram import filters
 
 other_filters = filters.group
 
 def command(commands: Union[str, List[str]]):
     return filters.command(commands, "")
 
-
-channel = "cczza" # Your Channel
-async def subscription(_, __: Client, message: Message):
-    user_id = message.from_user.id
-    try: await app.get_chat_member(channel, user_id)
-    except UserNotParticipant: return False
-    return True
-    
-subscribed = filters.create(subscription)
