7debfcfb8f8bfb746f6fb27e0a88dc81eb1650fb
Ema-02 <152199222+Ema-02@users.noreply.github.com>
2023-12-22 23:05:54 +0300

    Update song.py

--- FallenMusic/Modules/song.py
+++ FallenMusic/Modules/song.py
@@ -33,7 +33,7 @@ from youtube_search import YoutubeSearch
 from FallenMusic import BOT_MENTION, BOT_USERNAME, LOGGER, app
 
 
-@app.on_message(command(["/song", "/vsong", "/video", "/music", "بحث"] & subscribed, group=111)))
+@app.on_message(command(["/song", "/vsong", "/video", "/music", "بحث"]) & subscribed, group=111))
 async def song(_, message: Message):
     try:
         await message.delete()
